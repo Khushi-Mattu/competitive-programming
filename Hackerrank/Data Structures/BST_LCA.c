14b3460175b77eceb0ee25db2f74e055e09075ef
Khushi Mattu <82451914+Khushi-Mattu@users.noreply.github.com>
2021-12-10 08:58:59 +0530

    dsa

---
+++ Hackerrank/Data Structures/BST_LCA.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+struct node* insert( struct node* root, int data ) {
+		
+	if(root == NULL) {
+	
+        struct node* node = (struct node*)malloc(sizeof(struct node));
+
+        node->data = data;
+
+        node->left = NULL;
+        node->right = NULL;
+        return node;
+	  
+	} else {
+      
+		struct node* cur;
+		
+		if(data <= root->data) {
+            cur = insert(root->left, data);
+            root->left = cur;
+		} else {
+            cur = insert(root->right, data);
+            root->right = cur;
+		}
+	
+		return root;
+	}
+}
+
+/* you only have to complete the function given below.  
+node is defined as  
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+*/
+struct node *lca( struct node *root, int v1, int v2 ) {
+ while(root!= NULL){
+        if(v1 > root->data && v2 > root->data){
+            root = root->right;
+            
+        }else if(v1 < root->data && v2 <  root->data){
+            root = root ->left;
+        }else{
+            break;
+        }
+    }
+    return root;
+}
+
+
+int main() {
+  
+    struct node* root = NULL;
+    
+    int t;
+    int data;
+
+    scanf("%d", &t);
+
+    while(t-- > 0) {
+        scanf("%d", &data);
+        root = insert(root, data);
+    }
+  	int v1;
+  	int v2;
+  
+  	scanf("%d%d", &v1, &v2);
+	struct node *ans = lca(root, v1, v2);
+  	printf("%d", ans->data);
+  	
+    return 0;
+}
