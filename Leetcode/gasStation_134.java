c08400dd714a26d1be4cb60667701912e607bbd5
Khushi Mattu <82451914+Khushi-Mattu@users.noreply.github.com>
2022-01-21 21:00:50 +0530

    gas station leetcode

---
+++ Leetcode/gasStation_134.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n=gas.length;
+        int total=0;
+        int surplus=0;
+        int start=0;
+        for(int i=0;i<n;i++){
+            total+=gas[i]-cost[i];
+            surplus+=gas[i]-cost[i];
+            if(surplus<0){
+                surplus=0;
+                start=i+1;
+            }
+        }
+        return (total<0)?-1:start;
+    }
+}
