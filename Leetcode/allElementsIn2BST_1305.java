a57b54f3ad69bf7936b2554ed8b8a3518bc056f1
Khushi Mattu <82451914+Khushi-Mattu@users.noreply.github.com>
2022-01-27 09:53:35 +0530

    all elements in 2 bst leetcode day 6

---
+++ Leetcode/allElementsIn2BST_1305.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        
+        List<Integer> res = new ArrayList<>();
+        
+        while(root1 != null || root2 != null || !st1.empty() || !st2.empty()){
+            while(root1 != null){
+                st1.push(root1);
+                root1 = root1.left;
+            }
+            while(root2 != null){
+                st2.push(root2);
+                root2 = root2.left;
+            }
+            if(st2.empty() || (!st1.empty() && st1.peek().val <= st2.peek().val)){
+                root1 = st1.pop();
+                res.add(root1.val);
+                root1 = root1.right;
+            }
+            else{
+                root2 = st2.pop();
+                res.add(root2.val);
+                root2 = root2.right;
+            }
+        }
+        return res;
+    }
+}
