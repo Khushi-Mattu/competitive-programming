e271f6c4877cc0685e59d9176ea4df74ea5c5ef2
Khushi Mattu <khushi.mattu2020@vitstudent.ac.in>
2022-06-12 12:41:33 +0530

    fibonacci and tribonacci leetcode

---
+++ Leetcode/509_fibonacciNumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        int ans;
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        else{
+            ans=fib(n-1)+fib(n-2);
+        }
+        return ans;
+    }
+}
