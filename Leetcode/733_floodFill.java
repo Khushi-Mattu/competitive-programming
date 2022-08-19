8590a704521daf1f8030add2b947c3b8ef60600e
Khushi Mattu <khushi.mattu2020@vitstudent.ac.in>
2022-08-19 11:08:30 +0530

    leetcode flood fill

---
+++ Leetcode/733_floodFill.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        dfs(image, sr, sc, color, image[sr][sc]);
+        return image;
+    }
+    public void dfs(int[][] grid, int row, int col, int color, int prevColor){
+        if(row>=grid.length || col>=grid[0].length || row<0 || col <0 || grid[row][col] == color || grid[row][col]!=prevColor)
+            return;
+        grid[row][col] = color;
+        dfs(grid, row+1, col, color, prevColor);
+        dfs(grid, row, col+1, color, prevColor);
+        dfs(grid, row-1, col, color, prevColor);
+        dfs(grid, row, col-1, color, prevColor);
+    }
+}
