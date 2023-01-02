066f2a93be35e912094f8f62b0344d25c620b622
Khushi Mattu <khushi.mattu2020@vitstudent.ac.in>
2023-01-02 11:08:31 +0530

    520

--- Leetcode/520_detectCapital.java
+++ Leetcode/520_detectCapital.java
@@ -14,3 +14,39 @@ class Solution {
         return true;
     }
 }
+
+/*
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length()==1){
+            return true;
+        }
+        if(Character.isUpperCase(word.charAt(0))){
+            if(Character.isUpperCase(word.charAt(1))){
+                for(int i=1;i<word.length();i++)
+            {
+                if(Character.isLowerCase(word.charAt(i))){
+                    return false;
+                }
+            }
+            }
+            else{
+                for(int i=1;i<word.length();i++)
+            {
+                if(Character.isUpperCase(word.charAt(i))){
+                    return false;
+                }
+            }
+            }
+            
+        }
+        else{
+            for(int i=1;i<word.length();i++){
+                if(Character.isUpperCase(word.charAt(i))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
