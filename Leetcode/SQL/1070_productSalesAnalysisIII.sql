462167c8efeae5d3ac360cf4e91c4d541228785f
Khushi Mattu <khushi.mattu2020@vitstudent.ac.in>
2023-08-10 21:49:40 +0530

    7 sql questions 10/08/2023

---
+++ Leetcode/SQL/1070_productSalesAnalysisIII.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select product_id,year as first_year ,quantity,price from Sales where (product_id,year) in (select product_id,min(year) from Sales group by 1)
